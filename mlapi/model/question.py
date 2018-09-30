58dc18386030b038e72792a48bb4c31e5f0d58c1
xahor <erdnacram15@gmail.com>
2018-09-30 16:40:04 -0700

    updated question model + return hardcoded dummy question in endpoint

--- mlapi/model/question.py
+++ mlapi/model/question.py
@@ -1,7 +1,15 @@
 class Question(object):
 
-    def __init__(self, text):
-        self.text = text
+    def __init__(self, id, facet_name, facet_values, answer, status):
+        self.id = id
+        self.facet_name = facet_name
+        self.facet_values = facet_values
+        self.answer = answer
+        self.status = status
 
     def to_json(self):
-        return {"text": self.text}
+        return {"id": self.id,
+                "facetName": self.facet_name,
+                "facetValues": self.facet_values,
+                "answer": self.answer,
+                "status": self.status}
