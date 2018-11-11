10a82b7eeb859dd3c9232dc67f29bc26c7a2bbcc
Hajar <>
2018-11-11 10:55:13 -0500

    add sort to discriminating facets

--- test/discriminating_facets_test.py
+++ test/discriminating_facets_test.py
@@ -23,7 +23,8 @@ class TestDiscriminatingFacets(unittest.TestCase):
 
         discriminating_facets = discriminating_algo.get_discriminating_facets(facets_by_document)
         self.assertEqual(len(discriminating_facets), 1)
-        self.assertEqual(discriminating_facets['NameA'], ['ValueA1', 'ValueA2'])
+        self.assertEqual(discriminating_facets[0][0], 'NameA')
+        self.assertEqual(discriminating_facets[0][1], ['ValueA1', 'ValueA2'])
 
     def test_when_2_discriminating_facets_then_run_one_time_algorithm_and_return_2_facets(self):
         discriminating_algo = DiscriminatingFacetsAlgo()
@@ -37,8 +38,10 @@ class TestDiscriminatingFacets(unittest.TestCase):
 
         discriminating_facets = discriminating_algo.get_discriminating_facets(facets_by_document)
         self.assertEqual(len(discriminating_facets), 2)
-        self.assertEqual(discriminating_facets['NameA'], ['ValueA1', 'ValueA2'])
-        self.assertEqual(discriminating_facets['NameC'], ['ValueC1', 'ValueC2', 'ValueC3'])
+        self.assertEqual(discriminating_facets[0][0], 'NameC')
+        self.assertEqual(discriminating_facets[0][1], ['ValueC1', 'ValueC2', 'ValueC3'])
+        self.assertEqual(discriminating_facets[1][0], 'NameA')
+        self.assertEqual(discriminating_facets[1][1], ['ValueA1', 'ValueA2'])
 
     def test_when_facet_dont_have_3_documents_then_return_0_facet(self):
         discriminating_algo = DiscriminatingFacetsAlgo()
